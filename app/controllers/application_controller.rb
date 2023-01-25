70df9c55efe42b4f83d6f295db8f24904a4a5687
Appdevzach <zmarciano@uchicago.edu>
2023-01-25 19:49:20 +0000

    Final version all good

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,22 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
-  def play_rock
+  def homepage 
+    render ({ :template => "game_templates/rules.html.erb"})
+  end
 
-    #redirect_to("https://www.wikipedia.org")
+  def play_rock
     render ({:template => "game_templates/user_rock.html.erb"})
-  end 
+  end
+
+  def play_paper
+    render ({:template => "game_templates/user_paper.html.erb"})
+
+  end
+
+  def play_scissors
+    render ({:template => "game_templates/user_scissors.html.erb"})
+  end
+  
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  
+
+  get("/", { :controller => "application", :action => "homepage"})
+
   get("/rock", {:controller => "application", :action => "play_rock"})
+
+  get("/paper", {:controller => "application", :action => "play_paper"})
+
+  get("/scissors", {:controller => "application", :action => "play_scissors"})
 
 end
